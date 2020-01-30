20c2e5c5367b7af5e04528f0a4889662af7cf5d4
Michael Zigldrum <michael.zigldrum@gmail.com>
2020-01-30 22:38:05 +0100

    Increased fontsize for readability\nfixed paddings so writing is not clipped.

--- src/components/TrashPanel.vue
+++ src/components/TrashPanel.vue
@@ -4,32 +4,32 @@
     <v-row v-if="activeEvent !== undefined && activeEvent !== null" class="text-left mb-0">
         <v-list-item two-line>
           <v-list-item-icon>
-            <v-icon size="60px">mdi-trash-can-outline</v-icon>
+            <v-icon size="70px">mdi-trash-can-outline</v-icon>
           </v-list-item-icon>
           <v-list-item-content>
-            <v-list-item-title class="display-1">{{activeEvent.DESCRIPTION}} </v-list-item-title>
-            <v-list-item-subtitle class="headline">{{activeEvent.dateObj.getDate()}}.{{activeEvent.dateObj.getMonth() +1 }}.{{activeEvent.dateObj.getFullYear()}}</v-list-item-subtitle>
+            <v-list-item-title class="display-2 py-3">{{activeEvent.DESCRIPTION}} </v-list-item-title>
+            <v-list-item-subtitle class="display-1 py-2">{{activeEvent.dateObj.getDate()}}.{{activeEvent.dateObj.getMonth() +1 }}.{{activeEvent.dateObj.getFullYear()}}</v-list-item-subtitle>
           </v-list-item-content>
         </v-list-item>
     </v-row>
     <v-row v-else class="text-left mb-0">
         <v-list-item one-line dense>
           <v-list-item-icon>
-            <v-icon size="50px">mdi-trash-can-outline</v-icon>
+            <v-icon size="70px">mdi-trash-can-outline</v-icon>
           </v-list-item-icon>
           <v-list-item-content>
-            <v-list-item-title class="title">Keine aktuellen Termine</v-list-item-title>
+            <v-list-item-title class="display-1 py-4">Keine aktuellen Termine</v-list-item-title>
           </v-list-item-content>
         </v-list-item>    
     </v-row>
     <v-row align="start" class="text-left mt-0">
       <v-col>
         <v-timeline dense color="#000000">
-          <v-timeline-item small fill-dot v-for="event in eventList" :key="event.UID" class="pa-0" color="#cccccc">
+          <v-timeline-item fill-dot v-for="event in eventList" :key="event.UID" class="pa-0" color="#cccccc">
             <v-card color="#000000">
               <v-list-item>
                 <v-list-item-content>
-                  <v-list-item-title>
+                  <v-list-item-title class="headline py-2">
                     {{`${event.dateObj.getDate() >= 10 ? event.dateObj.getDate() : '0' + event.dateObj.getDate()}.
                     ${event.dateObj.getMonth() >= 9 ? (event.dateObj.getMonth() +1 ) : '0' + (event.dateObj.getMonth() +1 )}.
                     ${event.dateObj.getFullYear() >= 10 ? event.dateObj.getFullYear() : '0' + event.dateObj.getFullYear()}
@@ -84,6 +84,8 @@ export default {
         console.log('Detected active event!')
         this.activeEvent = events[0]
         events = events.slice(1)
+      } else {
+        this.activeEvent = null
       }
       this.eventList = events.slice(0, (events.length > 5 ? 5 : events.length))
     }
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -2,16 +2,16 @@
 <v-container fluid style="height: 100%">
   <v-col cols="12" style="height: 100%" align="end" align-self="end">
     <v-row align="start" justify="space-between" style="height: 100%">
-      <v-col xs="12" sm="6" md="4" lg="4" xl="4">
+      <v-col xs="12" sm="6" md="6" lg="4" xl="4">
         <!--<comicPanel/>
         <v-spacer></v-spacer>-->
         <trashPanel/>
       </v-col>
-      <v-col xs="12" sm="6" md="4" lg="4" xl="4">
+      <v-col xs="12" sm="6" md="6" lg="4" xl="4">
         <clockPanel/>
       </v-col>
       <v-spacer></v-spacer>
-      <v-col xs="12" sm="6" md="4" lg="3" xl="3">
+      <v-col xs="12" sm="6" md="6" lg="3" xl="3">
         <weatherPanel/>
       </v-col>
     </v-row>
